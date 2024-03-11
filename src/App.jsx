753edc14f8ff088af98f633257dbf0c958e22d59
Daniel Kanu <dkanu@myscholly.com>
2024-03-11 08:35:45 -0500

    testing vr

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,60 @@
-import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
+import "./App.css";
+import React, { Suspense, useLayoutEffect } from "react";
 import { Canvas } from "@react-three/fiber";
+import { useSpring, animated } from "@react-spring/three";
+import MyRotatingBox from "./components/my-rotating-box";
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
+import {
+  Center,
+  AccumulativeShadows,
+  RandomizedLight,
+  OrbitControls,
+  Environment,
+  useGLTF,
+} from "@react-three/drei";
 
 function App() {
   return (
     <>
       <VRButton />
-      <Canvas>
+      <Canvas flat dpr={[1, 2]} camera={{ fov: 75, position: [0, 0, 18] }}>
         <XR>
           <Controllers />
           <Hands />
-          <mesh>
-            <boxGeometry />
-            <meshBasicMaterial color="blue" />
-          </mesh>
+          <MyRotatingBox />
+          <color attach="background" args={["#e0b7ff"]} />
+          <ambientLight args={["#e0b7ff"]} intensity={3} />
+          <Center top>
+            <Suzi rotation={[-0.63, 0, 0]} scale={2} />
+          </Center>
+          <Center top position={[-2, 0, 1]}>
+            <mesh castShadow>
+              <sphereGeometry args={[0.25, 64, 64]} />
+              <meshStandardMaterial color="lightblue" />
+            </mesh>
+          </Center>
+          <gridHelper args={[10, 10, 10, 10, 10, 10]} />
+          <OrbitControls makeDefault />
         </XR>
       </Canvas>
     </>
   );
+}
+function Suzi(props) {
+  const { scene, materials } = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/suzanne-high-poly/model.gltf"
+  );
+  useLayoutEffect(() => {
+    scene.traverse(
+      (obj) => obj.isMesh && (obj.receiveShadow = obj.castShadow = true)
+    );
+    materials.default.color.set("orange");
+    materials.default.roughness = 0.2;
+    // materials.default.normalMap = new THREE.CanvasTexture(new FlakesTexture(), THREE.UVMapping, THREE.RepeatWrapping, THREE.RepeatWrapping)
+
+    materials.default.normalScale.set(0.1, 0.1);
+  });
+  return <primitive object={scene} {...props} />;
 }
 
 export default App;
